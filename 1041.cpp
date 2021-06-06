256b6384be7ed9c0e7dfc2fcbf5dfce511704c9f
Lara Guimaraes <laraguimaraes13@icloud.com>
2021-06-05 21:08:28 -0300

    esse c* n ta indo que odio

--- 1041.cpp
+++ 1041.cpp
@@ -1,23 +1,39 @@
 #include <stdio.h>
-int main(){
+int main()
+{
 
     float x, y;
     scanf("%f %f", &x, &y);
 
-    if (x == 0.0 && y == 0.0){
+    if (x == 0.0 && y == 0.0)
+    {
         printf("Origem\n");
-    } else if(x > 0 && y > 0){
-        printf("Q1\n");
-    } else if(x < 0 && y > 0){
+    }
+    else if (y == 0.0)
+    {
+        printf("Eixo X\n");
+    }
+    else if (x == 0)
+    {
+        printf("Eixo Y\n");
+    }
+
+    else if (x < 0.0 && y > 0.0)
+    {
         printf("Q2\n");
-    } else if(x < 0 && y < 0){
+    }
+    else if (x < 0.0 && y < 0.0)
+    {
         printf("Q3\n");
-    } else if(x > 0 && y < 0){
+    }
+    else if (x > 0.0 && y < 0.0)
+    {
         printf("Q4\n");
-    } else if(y == 0){
-        printf("Eixo x\n");
-    } else if(x == 0){
-        printf("Eixo y\n");
+    }
+
+    else
+    {
+        printf("Q1\n");
     }
     return 0;
-} 
+}
--- 1042.cpp
+++ 1042.cpp
@@ -1,23 +1,34 @@
 #include <stdio.h>
-int main(){
+int main()
+{
 
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    
-    if (a <= b && b <= c){
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", a, b, c, a, b, c);
-    } else if (b <= a && a <= c){
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", b, a, c, a, b, c);
-    } else if (a <= c && c <= b){
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", a, c, b, a, b, c);
-    }else if (b <= c && c <= a){
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", b, c, a, a, b, c);
-    } else if (c <= a && a <= b){
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", c, a, b, a, b, c);
-    } else {
-        printf("%d\n %d\n %d\n \n %d\n %d\n %d\n", a, b, a, a, b, c);
-    }
 
+    if (a < b && b < c)
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", a, b, c, a, b, c);
+    }
+    else if (b < a && a < c)
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", b, a, c, a, b, c);
+    }
+    else if (a < c && c < b)
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", a, c, b, a, b, c);
+    }
+    else if (b < c && c < a)
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", b, c, a, a, b, c);
+    }
+    else if (c < a && a < b)
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", c, a, b, a, b, c);
+    }
+    else
+    {
+        printf("\n%d\n%d\n%d\n \n%d\n%d\n%d\n", a, b, a, a, b, c);
+    }
 
     return 0;
 }
